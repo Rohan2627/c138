2a36c75d281c7cade79e1cb8daca4cb505f4dc24
Rohan2627 <72944737+Rohan2627@users.noreply.github.com>
2022-12-21 19:45:04 +0000

    Add files via upload

---
+++ Project138/procode.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+df1 = pd.read_csv('shared_articles.csv')
+df2 = pd.read_csv('users_interactions.csv')
+
+df1.columns = ['timestamp','eventType','contentId','authorPersonId', 'authorSessionId','authorUserAgent','authorRegion','authorCountry','contentType','url','title','text','lang']
+df2 = df2.merge(df1,on = 'contentId')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
